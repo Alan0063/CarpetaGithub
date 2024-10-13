6bb49529f08bb5b538f17da2bc06943a4968fb22
JhonatanJimenez24 <146896065+JhonatanJimenez24@users.noreply.github.com>
2024-10-12 22:11:04 -0600

    Add files via upload

--- ofbms_db.sql
+++ ofbms_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 09-10-2024 a las 23:11:32
+-- Tiempo de generación: 13-10-2024 a las 06:07:37
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -28,58 +28,59 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `admin` (
-  `admin_id` int(11) NOT NULL,
-  `admin_uname` varchar(20) NOT NULL,
-  `admin_email` varchar(50) NOT NULL,
-  `admin_pwd` varchar(100) NOT NULL
+  `id_admin` int(11) NOT NULL,
+  `nombre_admin` varchar(20) NOT NULL,
+  `email_admin` varchar(50) NOT NULL,
+  `pwd_admin` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Volcado de datos para la tabla `admin`
 --
 
-INSERT INTO `admin` (`admin_id`, `admin_uname`, `admin_email`, `admin_pwd`) VALUES
-(1, 'admin', 'admin@mail.com', '$2y$10$AFMhdlwEaWjjBzoCfdq62uNQqopNGW4vk8GXrDBNGKPAgB0mON0TC');
+INSERT INTO `admin` (`id_admin`, `nombre_admin`, `email_admin`, `pwd_admin`) VALUES
+(1, 'admin', 'admin@mail.com', '$2y$10$AFMhdlwEaWjjBzoCfdq62uNQqopNGW4vk8GXrDBNGKPAgB0mON0TC'),
+(2, 'josue', 'josue@gmail.com', '1234');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `airline`
+-- Estructura de tabla para la tabla `aerolinea`
 --
 
-CREATE TABLE `airline` (
-  `airline_id` int(11) NOT NULL,
-  `name` varchar(20) NOT NULL,
-  `seats` int(11) NOT NULL
+CREATE TABLE `aerolinea` (
+  `id_aerolinea` int(11) NOT NULL,
+  `nombre` varchar(20) NOT NULL,
+  `asientos` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Volcado de datos para la tabla `airline`
+-- Volcado de datos para la tabla `aerolinea`
 --
 
-INSERT INTO `airline` (`airline_id`, `name`, `seats`) VALUES
-(4, 'Air Hernandez', 200);
+INSERT INTO `aerolinea` (`id_aerolinea`, `nombre`, `asientos`) VALUES
+(6, 'UnachAir', 200);
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `cities`
+-- Estructura de tabla para la tabla `ciudad`
 --
 
-CREATE TABLE `cities` (
-  `city` varchar(20) NOT NULL
+CREATE TABLE `ciudad` (
+  `id_ciudad` int(25) NOT NULL,
+  `ciudad` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Volcado de datos para la tabla `cities`
+-- Volcado de datos para la tabla `ciudad`
 --
 
-INSERT INTO `cities` (`city`) VALUES
-('Metro Manila'),
-('Pampanga'),
-('Cebu'),
-('Iloilo'),
-('Bacolod');
+INSERT INTO `ciudad` (`id_ciudad`, `ciudad`) VALUES
+(6, 'Tuxtla Gutiérrez'),
+(7, 'Tamaulipas'),
+(8, 'Monterrey'),
+(9, 'Ciudad de México');
 
 -- --------------------------------------------------------
 
@@ -88,7 +89,7 @@ INSERT INTO `cities` (`city`) VALUES
 --
 
 CREATE TABLE `feedback` (
-  `feed_id` int(11) NOT NULL,
+  `id_feed` int(11) NOT NULL,
   `email` varchar(50) NOT NULL,
   `q1` varchar(250) NOT NULL,
   `q2` varchar(20) NOT NULL,
@@ -99,48 +100,41 @@ CREATE TABLE `feedback` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `flight`
+-- Estructura de tabla para la tabla `pago`
 --
 
-CREATE TABLE `flight` (
-  `flight_id` int(11) NOT NULL,
-  `admin_id` int(11) NOT NULL,
-  `arrivale` datetime NOT NULL,
-  `departure` datetime NOT NULL,
-  `Destination` varchar(20) NOT NULL,
-  `source` varchar(20) NOT NULL,
-  `airline` varchar(20) NOT NULL,
-  `Seats` varchar(110) NOT NULL,
-  `duration` varchar(20) NOT NULL,
-  `Price` int(11) NOT NULL,
-  `status` varchar(6) DEFAULT NULL,
-  `issue` varchar(50) DEFAULT NULL,
-  `last_seat` varchar(5) DEFAULT '',
-  `bus_seats` int(11) DEFAULT 20,
-  `last_bus_seat` varchar(5) DEFAULT ''
+CREATE TABLE `pago` (
+  `card_no` varchar(16) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `id_vuelo` int(11) NOT NULL,
+  `expire_date` varchar(5) DEFAULT NULL,
+  `monto` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Volcado de datos para la tabla `flight`
+-- Volcado de datos para la tabla `pago`
 --
 
-INSERT INTO `flight` (`flight_id`, `admin_id`, `arrivale`, `departure`, `Destination`, `source`, `airline`, `Seats`, `duration`, `Price`, `status`, `issue`, `last_seat`, `bus_seats`, `last_bus_seat`) VALUES
-(2, 1, '2022-11-22 08:30:00', '2022-11-22 07:30:00', 'Bacolod', 'Metro Manila', 'Cebu Pacific', '171', '1 Hour', 3500, '', '', '22C', 20, ''),
-(5, 1, '2022-11-21 17:00:00', '2022-11-21 16:00:00', 'Bacolod', 'Pampanga', 'AirAsia', '192', '1 Hour', 2500, 'issue', '60', '22B', 20, ''),
-(9, 1, '2024-10-10 21:58:00', '2024-10-10 15:58:00', 'Pampanga', 'Metro Manila', 'Air Hernandez', '197', '6 horas', 1500, '', '', '21C', 20, ''),
-(10, 1, '2024-10-12 12:20:00', '2024-10-11 00:19:00', 'Metro Manila', 'Pampanga', 'Air Hernandez', '200', '12 horas', 5000, '', '', '', 20, '');
+INSERT INTO `pago` (`card_no`, `id_usuario`, `id_vuelo`, `expire_date`, `monto`) VALUES
+('1111111111111111', 7, 11, '1111', 11),
+('1111111111111111', 7, 11, '1111', 11),
+('1111111111111111', 7, 11, '1111', 11),
+('1111111111111111', 7, 11, '1111', 11),
+('1111111111111111', 7, 11, '1111', 11),
+('1111111111111111', 7, 11, '1111', 11),
+('1111111111111111', 7, 11, '1111', 11);
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `passenger_profile`
+-- Estructura de tabla para la tabla `pasajero`
 --
 
-CREATE TABLE `passenger_profile` (
-  `passenger_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `flight_id` int(11) NOT NULL,
-  `mobile` varchar(110) NOT NULL,
+CREATE TABLE `pasajero` (
+  `id_pasajero` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `id_vuelo` int(11) NOT NULL,
+  `telefono` varchar(110) NOT NULL,
   `dob` datetime NOT NULL,
   `f_name` varchar(20) DEFAULT NULL,
   `m_name` varchar(20) DEFAULT NULL,
@@ -148,47 +142,12 @@ CREATE TABLE `passenger_profile` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Volcado de datos para la tabla `passenger_profile`
+-- Volcado de datos para la tabla `pasajero`
 --
 
-INSERT INTO `passenger_profile` (`passenger_id`, `user_id`, `flight_id`, `mobile`, `dob`, `f_name`, `m_name`, `l_name`) VALUES
-(2, 5, 2, '9123654789', '1997-06-23 00:00:00', 'Mark', 'D', 'Cooper'),
-(4, 5, 2, '9123564879', '1997-10-14 00:00:00', 'Jane ', 'C', 'Cooper'),
-(5, 5, 2, '9654123897', '1990-07-15 00:00:00', 'Mike', 'C', 'Williams'),
-(6, 6, 9, '1234567890', '2000-10-10 00:00:00', 'joshua', 'n', ' Hernandez Reyes'),
-(7, 6, 9, '1234567899', '2000-12-10 00:00:00', 'jose', 'm', 'perez'),
-(8, 6, 9, '1234567899', '2003-10-12 00:00:00', 'estrella', 'm', 'ailen');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `payment`
---
-
-CREATE TABLE `payment` (
-  `card_no` varchar(16) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `flight_id` int(11) NOT NULL,
-  `expire_date` varchar(5) DEFAULT NULL,
-  `amount` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
---
--- Volcado de datos para la tabla `payment`
---
-
-INSERT INTO `payment` (`card_no`, `user_id`, `flight_id`, `expire_date`, `amount`) VALUES
-('1234321211112222', 5, 5, '06/27', 2500),
-('1234654456455', 5, 2, '06/27', 3500),
-('4616182753905953', 5, 2, '06/27', 3500),
-('4616182753905953', 5, 2, '06/27', 3500),
-('4616182753905953', 5, 2, '06/27', 3500),
-('4616182753905953', 5, 2, '06/27', 3500),
-('4616182753905953', 5, 2, '06/27', 3500),
-('111112333311', 5, 2, '01/25', 3500),
-('1123456787654345', 6, 9, '1026', 1500),
-('1234567890098767', 6, 9, '1028', 1500),
-('0987654321234567', 6, 9, '12/26', 3000);
+INSERT INTO `pasajero` (`id_pasajero`, `id_usuario`, `id_vuelo`, `telefono`, `dob`, `f_name`, `m_name`, `l_name`) VALUES
+(1, 7, 11, '9921173224', '2005-01-12 00:00:00', 'josue', 'wilber', 'galvez roblero'),
+(2, 7, 11, '9921172324', '2004-01-22 00:00:00', 'josue', 'wilber', 'galvez roblero');
 
 -- --------------------------------------------------------
 
@@ -211,10 +170,10 @@ CREATE TABLE `pwdreset` (
 --
 
 CREATE TABLE `ticket` (
-  `ticket_id` int(11) NOT NULL,
-  `passenger_id` int(11) NOT NULL,
-  `flight_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
+  `id_ticket` int(11) NOT NULL,
+  `id_pasajero` int(11) NOT NULL,
+  `id_vuelo` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
   `seat_no` varchar(10) NOT NULL,
   `cost` int(11) NOT NULL,
   `class` varchar(3) NOT NULL
@@ -224,38 +183,70 @@ CREATE TABLE `ticket` (
 -- Volcado de datos para la tabla `ticket`
 --
 
-INSERT INTO `ticket` (`ticket_id`, `passenger_id`, `flight_id`, `user_id`, `seat_no`, `cost`, `class`) VALUES
-(1, 1, 5, 5, '22A', 2500, 'E'),
-(3, 1, 2, 5, '21A', 3500, 'E'),
-(9, 3, 2, 5, '22B', 3500, 'E'),
-(10, 5, 2, 5, '22C', 3500, 'E'),
-(12, 7, 9, 6, '21B', 1500, 'E'),
-(13, 8, 9, 6, '21C', 3000, 'E');
+INSERT INTO `ticket` (`id_ticket`, `id_pasajero`, `id_vuelo`, `id_usuario`, `seat_no`, `cost`, `class`) VALUES
+(14, 1, 11, 7, '21B', 11, 'E'),
+(15, 2, 11, 7, '21C', 11, 'E');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `users`
+-- Estructura de tabla para la tabla `usuario`
 --
 
-CREATE TABLE `users` (
-  `user_id` int(11) NOT NULL,
-  `username` varchar(20) NOT NULL,
+CREATE TABLE `usuario` (
+  `id_usuario` int(11) NOT NULL,
+  `nombre_usuario` varchar(20) NOT NULL,
   `email` varchar(50) NOT NULL,
   `password` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Volcado de datos para la tabla `users`
+-- Volcado de datos para la tabla `usuario`
 --
 
-INSERT INTO `users` (`user_id`, `username`, `email`, `password`) VALUES
-(1, 'christine', 'christine@mail.com', '$2y$10$KRXGkY.dxYjD8FLZclW/Tu04wl76lD7IA4Z3nAsxtpdZxHNbYo4ZW'),
-(2, 'henry', 'henry@mail.com', '$2y$10$KRXGkY.dxYjD8FLZclW/Tu04wl76lD7IA4Z3nAsxtpdZxHNbYo4ZW'),
+INSERT INTO `usuario` (`id_usuario`, `nombre_usuario`, `email`, `password`) VALUES
 (3, 'andre', 'andre@mail.com', '$2y$10$KRXGkY.dxYjD8FLZclW/Tu04wl76lD7IA4Z3nAsxtpdZxHNbYo4ZW'),
 (4, 'james', 'james@mail.com', '$2y$10$KRXGkY.dxYjD8FLZclW/Tu04wl76lD7IA4Z3nAsxtpdZxHNbYo4ZW'),
 (5, 'mcooper', 'mcooper@mail.com', '$2y$10$.FUiqi1YNqPBTxstbratouNEux7TsPPZ8/YOrV.Bd2JbVFD95G1nS'),
-(6, 'Joshua', 'prueba@gmail.com', '$2y$10$LbyfgqoHV/kCcPrQd.ug7u3ndhNLhOt1.QpwwxZ.VISBj4Mwmq4wa');
+(6, 'Joshua', 'prueba@gmail.com', '$2y$10$LbyfgqoHV/kCcPrQd.ug7u3ndhNLhOt1.QpwwxZ.VISBj4Mwmq4wa'),
+(7, 'Josue Galvez', 'eljosuexd1914@gmail.com', '$2y$10$nyznGer0yVIg8pbIy/HWk.HktbLku2kBRFJkoI9jJKL/9E/ALVf2S'),
+(8, 'javier', 'eljavi@gmail.com', '$2y$10$jW9UakWsWQeKKQG1CvwTVeQ6tQ7Zg25zrqgVkRvcAgVXBFJ8WGqne');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `vuelo`
+--
+
+CREATE TABLE `vuelo` (
+  `id_vuelo` int(11) NOT NULL,
+  `id_admin` int(11) NOT NULL,
+  `fecha_llegada` datetime NOT NULL,
+  `fecha_salida` datetime NOT NULL,
+  `destino` varchar(20) NOT NULL,
+  `salida` varchar(20) NOT NULL,
+  `aerolinea` varchar(20) NOT NULL,
+  `asientos` varchar(110) NOT NULL,
+  `duration` varchar(20) NOT NULL,
+  `precio` int(11) NOT NULL,
+  `status` varchar(6) DEFAULT NULL,
+  `issue` varchar(50) DEFAULT NULL,
+  `last_seat` varchar(5) DEFAULT '',
+  `bus_seats` int(11) DEFAULT 20,
+  `last_bus_seat` varchar(5) DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `vuelo`
+--
+
+INSERT INTO `vuelo` (`id_vuelo`, `id_admin`, `fecha_llegada`, `fecha_salida`, `destino`, `salida`, `aerolinea`, `asientos`, `duration`, `precio`, `status`, `issue`, `last_seat`, `bus_seats`, `last_bus_seat`) VALUES
+(11, 1, '2024-10-12 15:43:00', '2024-10-12 14:43:00', 'Tamaulipas', 'Tuxtla Gutiérrez', 'UnachAir', '197', '1', 11, '', '', '21C', 20, ''),
+(12, 1, '2024-10-13 20:37:00', '2024-10-13 08:37:00', 'Tamaulipas', 'Tuxtla Gutiérrez', 'UnachAir', '200', '12 horas', 1200, '', '', '', 20, ''),
+(13, 1, '2024-10-13 09:00:00', '2024-10-12 21:00:00', 'Tamaulipas', 'Tuxtla Gutiérrez', 'UnachAir', '200', '12 horas', 2147483647, '', '', '', 20, ''),
+(14, 1, '2024-12-01 12:00:00', '2024-12-01 10:00:00', 'Ciudad_Destino', 'Ciudad_Origen', 'Aerolínea_Prueba', '100', '2:00', 200, '', '', '', 20, ''),
+(15, 1, '2024-10-13 09:26:00', '2024-10-12 21:26:00', 'Tamaulipas', 'Monterrey', 'UnachAir', '200', '12 horas', 222222, '', '', '', 20, ''),
+(16, 1, '2024-10-13 09:35:00', '2024-10-12 09:35:00', 'Tuxtla Gutiérrez', 'Ciudad de México', 'UnachAir', '200', '12 horas', 3000, '', '', '', 20, '');
 
 --
 -- Índices para tablas volcadas
@@ -265,41 +256,40 @@ INSERT INTO `users` (`user_id`, `username`, `email`, `password`) VALUES
 -- Indices de la tabla `admin`
 --
 ALTER TABLE `admin`
-  ADD PRIMARY KEY (`admin_id`);
+  ADD PRIMARY KEY (`id_admin`);
 
 --
--- Indices de la tabla `airline`
+-- Indices de la tabla `aerolinea`
 --
-ALTER TABLE `airline`
-  ADD PRIMARY KEY (`airline_id`);
+ALTER TABLE `aerolinea`
+  ADD PRIMARY KEY (`id_aerolinea`);
+
+--
+-- Indices de la tabla `ciudad`
+--
+ALTER TABLE `ciudad`
+  ADD PRIMARY KEY (`id_ciudad`);
 
 --
 -- Indices de la tabla `feedback`
 --
 ALTER TABLE `feedback`
-  ADD PRIMARY KEY (`feed_id`);
+  ADD PRIMARY KEY (`id_feed`);
 
 --
--- Indices de la tabla `flight`
+-- Indices de la tabla `pago`
 --
-ALTER TABLE `flight`
-  ADD PRIMARY KEY (`flight_id`),
-  ADD KEY `admin_id` (`admin_id`);
+ALTER TABLE `pago`
+  ADD KEY `user_id` (`id_usuario`),
+  ADD KEY `vuelo` (`id_vuelo`);
 
 --
--- Indices de la tabla `passenger_profile`
+-- Indices de la tabla `pasajero`
 --
-ALTER TABLE `passenger_profile`
-  ADD PRIMARY KEY (`passenger_id`),
-  ADD KEY `user_id` (`user_id`),
-  ADD KEY `flight_id` (`flight_id`);
-
---
--- Indices de la tabla `payment`
---
-ALTER TABLE `payment`
-  ADD KEY `user_id` (`user_id`),
-  ADD KEY `flight_id` (`flight_id`);
+ALTER TABLE `pasajero`
+  ADD PRIMARY KEY (`id_pasajero`),
+  ADD KEY `user_id` (`id_usuario`),
+  ADD KEY `flight_id` (`id_vuelo`);
 
 --
 -- Indices de la tabla `pwdreset`
@@ -311,16 +301,23 @@ ALTER TABLE `pwdreset`
 -- Indices de la tabla `ticket`
 --
 ALTER TABLE `ticket`
-  ADD PRIMARY KEY (`ticket_id`),
-  ADD KEY `user_id` (`user_id`),
-  ADD KEY `flight_id` (`flight_id`),
-  ADD KEY `passenger_id` (`passenger_id`);
+  ADD PRIMARY KEY (`id_ticket`),
+  ADD KEY `user_id` (`id_usuario`),
+  ADD KEY `flight_id` (`id_vuelo`),
+  ADD KEY `passenger_id` (`id_pasajero`);
 
 --
--- Indices de la tabla `users`
+-- Indices de la tabla `usuario`
 --
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`user_id`);
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
+-- Indices de la tabla `vuelo`
+--
+ALTER TABLE `vuelo`
+  ADD PRIMARY KEY (`id_vuelo`),
+  ADD KEY `admin` (`id_admin`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -330,31 +327,31 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT de la tabla `airline`
+-- AUTO_INCREMENT de la tabla `aerolinea`
 --
-ALTER TABLE `airline`
-  MODIFY `airline_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+ALTER TABLE `aerolinea`
+  MODIFY `id_aerolinea` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `ciudad`
+--
+ALTER TABLE `ciudad`
+  MODIFY `id_ciudad` int(25) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de la tabla `feedback`
 --
 ALTER TABLE `feedback`
-  MODIFY `feed_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_feed` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `flight`
+-- AUTO_INCREMENT de la tabla `pasajero`
 --
-ALTER TABLE `flight`
-  MODIFY `flight_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
-
---
--- AUTO_INCREMENT de la tabla `passenger_profile`
---
-ALTER TABLE `passenger_profile`
-  MODIFY `passenger_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+ALTER TABLE `pasajero`
+  MODIFY `id_pasajero` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `pwdreset`
@@ -366,44 +363,50 @@ ALTER TABLE `pwdreset`
 -- AUTO_INCREMENT de la tabla `ticket`
 --
 ALTER TABLE `ticket`
-  MODIFY `ticket_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id_ticket` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
--- AUTO_INCREMENT de la tabla `users`
+-- AUTO_INCREMENT de la tabla `usuario`
 --
-ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+ALTER TABLE `usuario`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT de la tabla `vuelo`
+--
+ALTER TABLE `vuelo`
+  MODIFY `id_vuelo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- Restricciones para tablas volcadas
 --
 
 --
--- Filtros para la tabla `flight`
+-- Filtros para la tabla `pago`
 --
-ALTER TABLE `flight`
-  ADD CONSTRAINT `flight_ibfk_1` FOREIGN KEY (`admin_id`) REFERENCES `admin` (`admin_id`);
+ALTER TABLE `pago`
+  ADD CONSTRAINT `pago_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`),
+  ADD CONSTRAINT `vuelo` FOREIGN KEY (`id_vuelo`) REFERENCES `vuelo` (`id_vuelo`);
 
 --
--- Filtros para la tabla `passenger_profile`
+-- Filtros para la tabla `pasajero`
 --
-ALTER TABLE `passenger_profile`
-  ADD CONSTRAINT `passenger_profile_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `passenger_profile_ibfk_2` FOREIGN KEY (`flight_id`) REFERENCES `flight` (`flight_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Filtros para la tabla `payment`
---
-ALTER TABLE `payment`
-  ADD CONSTRAINT `payment_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
-  ADD CONSTRAINT `payment_ibfk_2` FOREIGN KEY (`flight_id`) REFERENCES `flight` (`flight_id`);
+ALTER TABLE `pasajero`
+  ADD CONSTRAINT `pasajero_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `pasajero_ibfk_2` FOREIGN KEY (`id_vuelo`) REFERENCES `vuelo` (`id_vuelo`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Filtros para la tabla `ticket`
 --
 ALTER TABLE `ticket`
-  ADD CONSTRAINT `ticket_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `ticket_ibfk_2` FOREIGN KEY (`flight_id`) REFERENCES `flight` (`flight_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `ticket_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `ticket_ibfk_2` FOREIGN KEY (`id_vuelo`) REFERENCES `vuelo` (`id_vuelo`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `vuelo`
+--
+ALTER TABLE `vuelo`
+  ADD CONSTRAINT `admin` FOREIGN KEY (`id_admin`) REFERENCES `admin` (`id_admin`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
